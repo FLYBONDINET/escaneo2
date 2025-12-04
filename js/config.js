a167b4a5bd9dbadfbc5d47147d7b9a067d13c081
FLYBONDINET <NICOLAS.EGUI@FLYBoNDI.COM>
2025-12-04 01:36:55 -0300

    nn

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // Poné tu WebApp URL de Google Apps Script (termina en /exec)
-const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwSH1qVSi9fG8grFiUZ7DSl9Zo8K7PiO25Yp54h7aXX_dVoJcoktgd92wUawiIBUYrT/exec"; // Ej: "https://script.google.com/macros/s/XXXX/exec"
+const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwa-B9DTDGvNZptLh_yw4Uu7ewwPV5fG01PvDBrEf4voPMiD4ukJ_z-p0DyRNe5gZMN/exec"; // Ej: "https://script.google.com/macros/s/XXXX/exec"
